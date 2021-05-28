990c13039073acd1ce8dc26a0f78d163c2f2cd0f
samanosuke-akechi <78411965+samanosuke-akechi@users.noreply.github.com>
2021-05-28 17:32:34 +0900

    PAY.JPから顧客情報を取得
    カード情報（トークン）を元に顧客情報を取得。
    customer.cards.firstでその顧客情報を元にカード番号などを取得する。
    firstは顧客が複数カードを登録している場合、そのうちの最初のカードを取得するということ。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
   def show
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # 環境変数を読み込む
-    card = Card.find_by(user_id: current_user.id)  # ユーザーのid情報を元に、カード情報を取得
+    card = Card.find_by(user_id: current_user.id)  # ユーザーのid情報を元に、カード情報(トークン)を取得
+
+    customer = Payjp::Customer.retrieve(card.customer_token)  # カード情報(トークン)を元に、顧客情報を取得
+    @card = customer.cards.first # カード情報(番号など)の取得
   end
 
   def update
