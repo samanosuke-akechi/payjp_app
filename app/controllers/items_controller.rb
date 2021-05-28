435418756dfc536f0e1494c82f6262ef7c748e92
samanosuke-akechi <78411965+samanosuke-akechi@users.noreply.github.com>
2021-05-28 18:25:53 +0900

    支払い情報のオブジェクト生成
    環境変数の読み込み。
    Payjp::Charge.createの形で生成。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,6 +7,14 @@ class ItemsController < ApplicationController
 
   def order  # 購入するときのアクション
     redirect_to new_card_path and return unless current_user.card.present?
+
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"] # 環境変数を読み込む
+    customer_token = current_user.card.customer_token # ログインしているユーザーの顧客トークンを定義
+    Payjp::Charge.create(
+      amount: @item.price, # 商品の値段
+      customer: customer_token, # 顧客のトークン
+      currency: 'jpy' # 通貨の種類（日本円）
+    )
   end
   
   private
