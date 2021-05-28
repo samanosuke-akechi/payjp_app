c7c0a5e0d37d2888940cba5ae9d74f73acbb8ba7
samanosuke-akechi <78411965+samanosuke-akechi@users.noreply.github.com>
2021-05-28 18:19:24 +0900

    購入する商品の特定、カード未登録時にカード登録画面に遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,17 @@
 class ItemsController < ApplicationController
+  before_action :find_item, only: :order
+  
   def index
     @items = Item.all
+  end
+
+  def order  # 購入するときのアクション
+    redirect_to new_card_path and return unless current_user.card.present?
+  end
+  
+  private
+
+  def find_item
+    @item = Item.find(params[:id])
   end
 end
